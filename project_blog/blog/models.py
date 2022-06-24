3a7ae7110c081501f3f57846a4675aea3c0972fa
trananha <trananha2810@gmail.com>
2022-06-24 17:13:11 +0700

    Define Blog models

---
+++ project_blog/blog/models.py
@@ -0,0 +1,36 @@
+from plistlib import UID
+from django.db import models
+
+class Blog(models.Model):
+    uid = models.UUIDField(primary_key = True, editable = False, max_length=36)
+    name = models.TextField(max_length=255)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(null=True)
+    is_published = models.BooleanField()
+    published_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True)
+    
+class BlogHistory(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    revision = models.IntegerField()
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField(max_length=255)
+    content = models.TextField(null=True)
+    is_published = models.BooleanField()
+    published_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True)
+    
+    # class Meta:
+    #     unique_together = ('blog', 'revision',)
+    
+class BlogLike(models.Model):
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment = models.TextField(null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(null=True)
+    
+    # class Meta:
+    #     unique_together = ('author', 'blog',)
