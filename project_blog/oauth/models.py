f046eab94a3b79b17df337720e3f542ce2e8fbf4
Hoàng Phúc <hoangphuc.osa@gmail.com>
2022-06-27 13:50:48 +0700

    Update oauth models

--- project_blog/oauth/models.py
+++ project_blog/oauth/models.py
@@ -1,30 +1,117 @@
 from django.db import models
-
+from user_account.models import User
 
 # Create your models here.
 class UserDeviceToken(models.Model):
-    # author = models.ForeignKey('User', unique=True, on_delete=models.CASCADE)
-    token = models.CharField(max_length=255, unique=True)
-    device_id = models.CharField(max_length=255, null=True)
-    device_token = models.CharField(max_length=255, null=True)
-    device_model = models.CharField(max_length=255, null=True)
-    user_agent = models.TextField(null=True)
+    author = models.ForeignKey(
+        to=User, 
+        unique=True, 
+        on_delete=models.CASCADE,
+        to_field='id',
+        related_name='user_device_token_fk_author',
+        db_column='author_id',
+        db_constraint=False, # accessing a related object that doesn’t exist will raise DoesNotExist 
+        null=False,
+        blank=False
+    )
+
+    token = models.CharField(
+        max_length=255, 
+        unique=True,
+    )
+
+    device_id = models.CharField(
+        max_length=255, 
+        null=True,
+        blank=True,
+    )
+
+    device_token = models.CharField(
+        max_length=255, 
+        null=True,
+        blank=True,
+    )
+
+    device_model = models.CharField(
+        max_length=255, 
+        null=True,
+    )
+
+    user_agent = models.TextField(
+        null=True,
+        blank=True,
+    )
+
     active = models.BooleanField()
-    created_at = models.DateTimeField()
-    deactivated_at = models.DateTimeField(null=True)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    deactivated_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        auto_now=True,
+    )
 
 class UserActivation(models.Model):
-    # author = models.ForeignKey('User', unique=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        to=User, 
+        unique=True, 
+        on_delete=models.CASCADE,
+        to_field='id',
+        related_name='user_activation_fk_author',
+        db_column='author_id',
+        db_constraint=False,
+        null=False,
+        blank=False,
+    )
+
     active = models.BooleanField()
-    token = models.CharField(max_length=255, unique=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(null=True)
+
+    token = models.CharField(
+        max_length=255, 
+        unique=True,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        null=True,
+        blank=True,
+    )
 
 class ResetPassword():
-    # author = models.ForeignKey('User', unique=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        to=User, 
+        unique=True, 
+        on_delete=models.CASCADE,
+        to_field='id',
+        related_name='reset_password_fk_author',
+        db_column='author_id',
+        db_constraint=False,
+        null=False,
+        blank=False,
+    )
+
     active = models.BooleanField()
-    token = models.CharField(max_length=255, unique=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(null=True)
+
+    token = models.CharField(
+        max_length=255, 
+        unique=True,
+    )
+    
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        null=True,
+        blank=True,
+    )
 
 
