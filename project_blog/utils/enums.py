64b9c427a4ba88d2a4c41d2a8b3087c1df8bdc94
an.le123456 <an.le123456@hcmut.edu.vn>
2022-06-27 14:41:57 +0700

    fix Notification, User_Account, Emails_Logs

--- project_blog/utils/enums.py
+++ project_blog/utils/enums.py
@@ -9,6 +9,18 @@ class Gender(models.TextChoices):
     OTHER = 'OTHER'
 
 @unique
+class Status(models.TextChoices):
+    AVAILABLE='AVAILABLE'
+    LOCKED = 'LOCKED'
+    REMOVED = 'REMOVED'
+
+@unique
+class Type(models.TextChoices):
+    ACTIVATE= 'ACTIVATE' 
+    RESET_PASSWORD= 'RESET_PASSWORD' 
+    FOLLOWER_POST= 'FOLLOWER_POST'
+
+@unique
 class Attachment_type(models.TextChoices):
     AVA = 'AVATAR'
     CVR = 'BLOG_COVER'
