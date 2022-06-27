8a203301a0c5d484c8e851682844b35fdc2431bf
trananha <trananha2810@gmail.com>
2022-06-27 13:33:18 +0700

    Redefine Blog models

--- project_blog/blog/models.py
+++ project_blog/blog/models.py
@@ -1,39 +1,162 @@
-from plistlib import UID
+from pickle import FALSE, TRUE
+from turtle import back
 from django.db import models
+import uuid
+from user_account.models import User
 
 class Blog(models.Model):
     uid = models.UUIDField(
-        primary_key=True, editable=False, max_length=36, unique=True)
-    name = models.TextField(max_length=255)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField(null=True)
-    is_published = models.BooleanField()
-    published_at = models.DateTimeField(null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(null=True)
+        default=uuid.uuid4(),
+        primary_key=True,
+        editable=False, 
+        unique=True, 
+        max_length=36,
+        null=False,
+        blank=False,
+    )
+    name = models.TextField(
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    author = models.ForeignKey(
+        to=User, 
+        on_delete=models.CASCADE,
+        to_field='id',
+        related_name='blog_fk_author',
+        db_column='author_id',
+        db_constraint=False,
+        null=False,
+        blank=False,
+    )
+    content = models.TextField(
+        null=True,
+        blank=True,
+    )
+    is_published = models.BooleanField(
+        null=False,
+        blank=False,
+    )
+    published_at = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now=False, 
+        auto_now_add=True,
+        null=False,
+        blank=False,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        auto_now_add=False,
+        null=True,
+        blank=True,
+    )
 
 
 class BlogHistory(models.Model):
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    revision = models.IntegerField()
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.TextField(max_length=255)
-    content = models.TextField(null=True)
-    is_published = models.BooleanField()
-    published_at = models.DateTimeField(null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(null=True)
+    blog = models.ForeignKey(
+        to=Blog, 
+        on_delete=models.CASCADE,
+        to_field='uid',
+        related_name='bloghistory_fk_blog',
+        db_column='blog_id',
+        db_constraint=False,
+        null=False,
+        blank=False,
+    )
+    revision = models.IntegerField(
+        null=False,
+        blank=False,
+    )
+    author = models.ForeignKey(
+        to=User, 
+        on_delete=models.CASCADE,
+        to_field='id',
+        related_name='bloghistory_fk_author',
+        db_column='author_id',
+        db_constraint=False,
+        null=False,
+        blank=False,
+    )
+    name = models.TextField(
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    content = models.TextField(
+        null=True,
+        blank=True,
+    )
+    is_published = models.BooleanField(
+        null=False,
+        blank=False,
+    )
+    published_at = models.DateTimeField(
+        auto_now=True, 
+        auto_now_add=False,
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now=False, 
+        auto_now_add=True,
+        null=False,
+        blank=False,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        auto_now_add=False,
+        null=True,
+        blank=True,
+    )
 
-    # class Meta:
-    #     unique_together = ('blog', 'revision',)
+    class Meta:
+        unique_together = (
+            ('blog', 'revision',),
+        )
 
 
 class BlogLike(models.Model):
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    comment = models.TextField(null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(null=True)
-
-    # class Meta:
-    #     unique_together = ('author', 'blog',)
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        to_field='id',
+        related_name='bloglike_fk_author',
+        db_column='author_id',
+        db_constraint=False,
+        null=False,
+        blank=False,
+    )
+    blog = models.ForeignKey(
+        to=Blog, 
+        on_delete=models.CASCADE,
+        to_field='uid',
+        related_name='bloglike_fk_blog',
+        db_column='blog_id',
+        db_constraint=False,
+        null=False,
+        blank=False,
+    )
+    comment = models.TextField(
+        null=True, 
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now=False, 
+        auto_now_add=True,
+        null=False,
+        blank=False,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        auto_now_add=False,
+        null=True,
+        blank=True,
+    )
+    
+    class Meta:
+        unique_together = (
+            ('author', 'blog',),
+        )
