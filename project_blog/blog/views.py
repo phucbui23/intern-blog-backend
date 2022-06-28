48b39d7893c6874ba0a4c808c9eefabaebf187af
Hoàng Phúc <hoangphuc.osa@gmail.com>
2022-06-28 16:54:03 +0700

    Define create_blog API

--- project_blog/blog/views.py
+++ project_blog/blog/views.py
@@ -1,3 +1,39 @@
+# from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+# from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+
+from .models import Blog, BlogHistory, BlogLike
+from .serializers import (BlogHistorySerializer, BlogLikeSerializer,
+                          BlogSerializer)
+
 
 # Create your views here.
+@api_view(['POST'])
+def create_blog(request):
+    data = request.data
+    serializer = BlogSerializer(data=data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['POST'])
+# def create_blog_history(request):
+#     data = JSONParser().parse(request)
+#     serializer = BlogHistorySerializer(data=data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+#     return JsonResponse(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['POST'])
+# def create_blog_like(request):
+#     data = JSONParser().parse(request)
+#     serializer = BlogLikeSerializer(data=data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+#     return JsonResponse(serializer.data, status=status.HTTP_400_BAD_REQUEST)
--- project_blog/project_blog/settings.py
+++ project_blog/project_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cvkqf4x_zg5--)q6^o5z8nfwm!+$0v31co+dbr%vmm2oq_#a7x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'notification',
     'tag',
     'blog',
@@ -130,3 +131,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user_account.User'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
