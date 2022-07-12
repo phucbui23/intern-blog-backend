511c65c2d1b42f735d7ec3206ad5cebd177b293a
trananha <trananha2810@gmail.com>
2022-07-12 14:04:08 +0700

    get detailed likes of a blog

--- project_blog/blog/urls.py
+++ project_blog/blog/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('delete/', views.delete_blog),
     path('my_blog/', views.get_user_blog),
     path('follower_blog/', views.get_follower_blog),
-    path('new_blogs/', views.get_new_blog)
+    path('new_blogs/', views.get_new_blog),
+    path('get_likes/', views.get_blog_likes),
 ]
